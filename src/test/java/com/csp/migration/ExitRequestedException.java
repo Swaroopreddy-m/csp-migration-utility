3eae3bcc3884705ca5ebb6e783ded1eeaacf2d5a
Swaroopreddy-m <swaroopreddy963@gmail.com>
2026-06-08 13:04:19 +0530

    deleted tests for maven smooth compile with src/test

--- src/test/java/com/csp/migration/ExitRequestedException.java
+++
@@ -1,7 +0,0 @@
-package com.csp.migration.exception;
-
-public class ExitRequestedException extends Exception {
-    public ExitRequestedException(String message) {
-        super(message);
-    }
-}
